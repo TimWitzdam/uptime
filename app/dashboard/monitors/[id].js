f0b9bc1cc13d454376148611fa215adf2e05e50b
timwitzdam <tim2013w@gmail.com>
2023-08-23 18:26:36 +0200

    Added popup-menu + functionality for rename/delete

--- app/dashboard/monitors/[id].js
+++ app/dashboard/monitors/[id].js
@@ -14,7 +14,11 @@ import * as SecureStore from "expo-secure-store";
 import Globe from "../../../assets/globe.js";
 import Cursor from "../../../assets/cursor.js";
 import ShortArrow from "../../../assets/short-arrow.js";
+import RotatedArrow from "../../../assets/rotated-arrow.js";
 import { LineChart } from "react-native-gifted-charts";
+import { useActionSheet } from "@expo/react-native-action-sheet";
+import Toast from "react-native-root-toast";
+import Dialog from "react-native-dialog";
 
 export default function App() {
   const [currentTimeSelected, setCurrentTimeSelected] = useState(7);
@@ -22,6 +26,8 @@ export default function App() {
   const [fetched, setFetched] = useState(false);
   const [yAxisOffset, setYAxisOffset] = useState(null);
   const { id } = useLocalSearchParams();
+  const [renameVisible, setRenameVisible] = useState(false);
+  const [deleteVisible, setDeleteVisible] = useState(false);
 
   useEffect(() => {
     async function fetchData() {
@@ -43,6 +49,15 @@ export default function App() {
         if (data.status === "ok") {
           setMonitorDetails(data.monitor_details);
           setFetched(true);
+        } else {
+          Toast.show(
+            "Something went wrong. Please use the support page in the settings.",
+            {
+              duration: Toast.durations.LONG,
+              position: Toast.positions.BOTTOM,
+              backgroundColor: "#FF0000",
+            }
+          );
         }
       }
     }
@@ -80,14 +95,158 @@ export default function App() {
     setYAxisOffset(offset);
   }
 
+  async function saveNewName() {
+    const user_id = await SecureStore.getItemAsync("user_id");
+    const res = await fetch(
+      `http://192.168.178.33:5000/user/${user_id}/monitors/${monitorDetails.metadata.id}`,
+      {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          "Auth-Token": await SecureStore.getItemAsync("auth_token"),
+        },
+        body: JSON.stringify({
+          name: monitorDetails.metadata.name,
+        }),
+      }
+    );
+
+    const data = await res.json();
+
+    if (data.status === "ok") {
+      setRenameVisible(false);
+    } else {
+      Toast.show(
+        "Something went wrong. Please use the support page in the settings.",
+        {
+          duration: Toast.durations.LONG,
+          position: Toast.positions.BOTTOM,
+          backgroundColor: "#FF0000",
+        }
+      );
+    }
+  }
+
+  async function deleteMonitor() {
+    const user_id = await SecureStore.getItemAsync("user_id");
+    const res = await fetch(
+      `http://192.168.178.33:5000/user/${user_id}/monitors/${monitorDetails.metadata.id}`,
+      {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          "Auth-Token": await SecureStore.getItemAsync("auth_token"),
+        },
+      }
+    );
+
+    const data = await res.json();
+
+    if (data.status === "ok") {
+      router.replace("/dashboard/monitors");
+    } else {
+      Toast.show(
+        "Something went wrong. Please use the support page in the settings.",
+        {
+          duration: Toast.durations.LONG,
+          position: Toast.positions.BOTTOM,
+          backgroundColor: "#FF0000",
+        }
+      );
+    }
+  }
+  const { showActionSheetWithOptions } = useActionSheet();
+
+  const onPress = async () => {
+    const options = ["Delete", "Rename", "Cancel"];
+    const destructiveButtonIndex = 0;
+    const cancelButtonIndex = 2;
+
+    const selectedIndex = await new Promise((resolve) => {
+      showActionSheetWithOptions(
+        {
+          options,
+          cancelButtonIndex,
+          destructiveButtonIndex,
+        },
+        (index) => {
+          resolve(index);
+        }
+      );
+    });
+
+    switch (selectedIndex) {
+      case 1:
+        setRenameVisible(true);
+        break;
+
+      case destructiveButtonIndex:
+        setDeleteVisible(true);
+        break;
+
+      case cancelButtonIndex:
+        // Cancel
+        break;
+    }
+  };
+
   return (
     <View className=" bg-background flex-1 justify-between">
       {fetched ? (
         <ScrollView className="p-6">
-          <View className="w-full mb-6">
-            <View>
-              <Text>Go back</Text>
-            </View>
+          <Dialog.Container visible={renameVisible}>
+            <Dialog.Title>Rename monitor</Dialog.Title>
+            <Dialog.Description>
+              What do you want to rename this monitor to?
+            </Dialog.Description>
+            <Dialog.Input
+              onChangeText={(text) => {
+                setMonitorDetails({
+                  ...monitorDetails,
+                  metadata: {
+                    ...monitorDetails.metadata,
+                    name: text,
+                  },
+                });
+              }}
+            />
+            <Dialog.Button
+              label="Cancel"
+              onPress={() => setRenameVisible(false)}
+            />
+            <Dialog.Button label="Confirm" onPress={() => saveNewName()} />
+          </Dialog.Container>
+          <Dialog.Container visible={deleteVisible}>
+            <Dialog.Title>Delete monitor</Dialog.Title>
+            <Dialog.Description>
+              Are you sure you want to delete this monitor? This action cannot
+              be undone.
+            </Dialog.Description>
+            <Dialog.Button
+              label="Cancel"
+              onPress={() => setDeleteVisible(false)}
+            />
+            <Dialog.Button
+              label="Confirm"
+              color={"#C21717"}
+              onPress={() => deleteMonitor()}
+            />
+          </Dialog.Container>
+          <View className="w-full items-center flex-row justify-between">
+            <TouchableOpacity
+              className="mb-6 flex-row items-center"
+              onPress={() => router.replace("/dashboard/monitors")}
+            >
+              <RotatedArrow width="30px" />
+              <Text className="text-white ml-2 font-medium text-lg">
+                Go back
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={onPress}>
+              <Text className="font-bold text-white text-2xl rotate-90">
+                ...
+              </Text>
+            </TouchableOpacity>
           </View>
 
           <View className="w-full mb-4">
@@ -252,7 +411,6 @@ export default function App() {
           <View className="mb-10"></View>
         </ScrollView>
       ) : null}
-      <StatusBar style="auto" />
     </View>
   );
 }

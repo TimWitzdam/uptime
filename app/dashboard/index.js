6c8a7c6aded773b7e59722f0c26ce3bd9f2b1037
timwitzdam <tim2013w@gmail.com>
2023-08-24 14:16:31 +0200

    Added functionality to start page details

--- app/dashboard/index.js
+++ app/dashboard/index.js
@@ -1,17 +1,65 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Text, TouchableOpacity, View, TextInput } from "react-native";
 import { router } from "expo-router";
 import User from "../../assets/user.js";
 import Notification from "../../assets/notification.js";
 import Arrow from "../../assets/arrow.js";
-import Home from "../../assets/home.js";
-import Server from "../../assets/server.js";
-import Add from "../../assets/add.js";
-import List from "../../assets/list.js";
-import Settings from "../../assets/settings.js";
+import * as SecureStore from "expo-secure-store";
 
 export default function App() {
+  const [monitors, setMonitors] = useState([]);
+  const [fetched, setFetched] = useState(false);
+  const [userData, setUserData] = useState({});
+
+  useEffect(() => {
+    async function fetchMonitors() {
+      if (fetched) return;
+      const response = await fetch(
+        `http://192.168.178.33:5000/user/${await SecureStore.getItemAsync(
+          "user_id"
+        )}/monitors?limit=2`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            "Auth-Token": await SecureStore.getItemAsync("auth_token"),
+          },
+        }
+      );
+      if (response.status === 200) {
+        const data = await response.json();
+        if (data.status === "ok") {
+          setMonitors(data.monitor_details);
+          setFetched(true);
+        }
+      }
+    }
+    async function fetchUptime() {
+      if (fetched) return;
+      const response = await fetch(
+        `http://192.168.178.33:5000/user/${await SecureStore.getItemAsync(
+          "user_id"
+        )}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            "Auth-Token": await SecureStore.getItemAsync("auth_token"),
+          },
+        }
+      );
+      if (response.status === 200) {
+        const data = await response.json();
+        if (data.status === "ok") {
+          setUserData(data.user);
+        }
+      }
+    }
+    fetchMonitors();
+    fetchUptime();
+  });
+
   return (
     <View className=" bg-background flex-1 justify-between">
       <View className=" items-center p-6">
@@ -32,10 +80,21 @@ export default function App() {
           </View>
         </View>
         <View className="w-full mb-10">
-          <Text className="font-semibold text-4xl text-white">
-            Everything is
-          </Text>
-          <Text className="font-semibold text-4xl text-accent">up 🎉</Text>
+          {userData.down_within_last_5_minutes ? (
+            <View>
+              <Text className="font-semibold text-4xl text-white">
+                Some sites are
+              </Text>
+              <Text className="font-semibold text-4xl text-red">down 🔴</Text>
+            </View>
+          ) : (
+            <View>
+              <Text className="font-semibold text-4xl text-white">
+                Everything is
+              </Text>
+              <Text className="font-semibold text-4xl text-accent">up 🎉</Text>
+            </View>
+          )}
         </View>
         <View className="w-full flex flex-row">
           <View className="bg-[#212836] rounded-xl p-4 w-full mr-4">
@@ -43,66 +102,68 @@ export default function App() {
               Overall last 7 days
             </Text>
             <View className="flex flex-row items-center gap-2">
-              <Text className="text-white text-4xl font-semibold">100%</Text>
-              <View className="w-8 h-8 bg-green rounded-full"></View>
+              <Text className="text-white text-4xl font-semibold">
+                {userData.uptime}%
+              </Text>
+              <View
+                className={`w-8 h-8 rounded-full bg-${
+                  userData.uptime === 100 || userData.uptime >= 99
+                    ? "green"
+                    : userData.uptime >= 95
+                    ? "yellow"
+                    : "red"
+                }`}
+              ></View>
             </View>
             <Text className="text-text text-xl font-semibold mb-2">
               Your monitors
             </Text>
             <View>
-              <View className="flex flex-row gap-1 mb-2">
-                <View className="w-5 h-5 bg-accent rounded-lg "></View>
-                <View className="flex flex-row justify-between w-full flex-1">
-                  <View>
-                    <Text className="text-white text-lg font-semibold leading-5">
-                      Favourite 1
-                    </Text>
-                    <Text className="text-text">agencylabs.ai</Text>
-                  </View>
-                  <View className="flex flex-row gap-3">
+              {monitors.map((monitor) => (
+                <TouchableOpacity
+                  className="flex flex-row gap-1 mb-2"
+                  key={monitor.id}
+                  onPress={() =>
+                    router.replace(`/dashboard/monitors/${monitor.id}`)
+                  }
+                >
+                  <View className="w-5 h-5 bg-accent rounded-lg "></View>
+                  <View className="flex flex-row justify-between w-full flex-1">
                     <View>
-                      <Text className="text-xs text-text">Ping</Text>
-                      <Text className="text-white">39ms</Text>
+                      <Text className="text-white text-lg font-semibold leading-5">
+                        {monitor.name}
+                      </Text>
+                      <Text className="text-text">{monitor.url}</Text>
                     </View>
-                    <View>
-                      <Text className="text-xs text-text">Uptime</Text>
-                      <Text className="text-white">99,8%</Text>
-                    </View>
-                    <View>
-                      <Text className="text-xs text-text">Status</Text>
-                      <Text className="text-yellow">Slow</Text>
-                    </View>
-                  </View>
-                </View>
-              </View>
-              <View className="flex flex-row gap-1">
-                <View className="w-5 h-5 bg-accent rounded-lg "></View>
-                <View className="flex flex-row justify-between w-full flex-1">
-                  <View className="w-[50%]">
-                    <Text
-                      className="text-white text-lg font-semibold leading-5 overflow-ellipsis truncate"
-                      numberOfLines={1}
-                    >
-                      Favourite 2asdasdasdasdsadsad
-                    </Text>
-                    <Text className="text-text">agencylabs.ai</Text>
-                  </View>
-                  <View className="flex flex-row gap-3">
-                    <View>
-                      <Text className="text-xs text-text">Ping</Text>
-                      <Text className="text-white">39ms</Text>
-                    </View>
-                    <View>
-                      <Text className="text-xs text-text">Uptime</Text>
-                      <Text className="text-white">99,8%</Text>
-                    </View>
-                    <View>
-                      <Text className="text-xs text-text">Status</Text>
-                      <Text className="text-yellow">Slow</Text>
+                    <View className="flex flex-row gap-3">
+                      <View>
+                        <Text className="text-xs text-text">Ping</Text>
+                        <Text className="text-white">
+                          {monitor.response_time}ms
+                        </Text>
+                      </View>
+                      <View>
+                        <Text className="text-xs text-text">Uptime</Text>
+                        <Text className="text-white">{monitor.uptime}%</Text>
+                      </View>
+                      <View>
+                        <Text className="text-xs text-text">Status</Text>
+                        <Text
+                          className={`text-${
+                            monitor.status.toLowerCase() === "up"
+                              ? "green"
+                              : monitor.status.toLowerCase() === "down"
+                              ? "red"
+                              : "yellow"
+                          }`}
+                        >
+                          {monitor.status}
+                        </Text>
+                      </View>
                     </View>
                   </View>
-                </View>
-              </View>
+                </TouchableOpacity>
+              ))}
             </View>
             <View className="w-full flex flex-row items-center mt-2">
               <Text className="text-white font-semibold ml-auto mr-2">

1ebdca53e2a61c44ac4edf5a2b21f92b6a06996a
timwitzdam <tim2013w@gmail.com>
2023-08-25 11:55:06 +0200

    Added settings page

---
+++ app/dashboard/settings/index.js
@@ -0,0 +1,94 @@
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import {
+  Text,
+  TouchableOpacity,
+  View,
+  TextInput,
+  ScrollView,
+  Linking,
+} from "react-native";
+import { router } from "expo-router";
+import Support from "../../../assets/support.js";
+import Arrow from "../../../assets/arrow.js";
+import RotatedArrow from "../../../assets/rotated-arrow.js";
+import LogOut from "../../../assets/logout.js";
+import * as SecureStore from "expo-secure-store";
+import Toast from "react-native-root-toast";
+
+export default function App() {
+  async function logOutUser() {
+    const response = await fetch(`http://192.168.178.33:5000/logout`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Auth-Token": await SecureStore.getItemAsync("auth_token"),
+      },
+      body: JSON.stringify({
+        user_id: await SecureStore.getItemAsync("user_id"),
+      }),
+    });
+    if (response.status === 200) {
+      const data = await response.json();
+      if (data.status === "ok") {
+        SecureStore.deleteItemAsync("auth_token");
+        SecureStore.deleteItemAsync("user_id");
+        router.replace("/");
+      } else {
+        Toast.show(
+          "Something went wrong. Please use the support page in the settings.",
+          {
+            duration: Toast.durations.LONG,
+            position: Toast.positions.BOTTOM,
+            backgroundColor: "#FF0000",
+          }
+        );
+      }
+    }
+  }
+  return (
+    <View className=" bg-background flex-1 justify-between">
+      <View className=" items-center p-6">
+        <View className="w-full items-center flex-row justify-between">
+          <TouchableOpacity
+            className="mb-6 flex-row items-center"
+            onPress={() => router.back()}
+          >
+            <RotatedArrow width="30px" />
+            <Text className="text-white ml-2 font-medium text-lg">Go back</Text>
+          </TouchableOpacity>
+        </View>
+        <View className="w-full mb-4">
+          <Text className="font-semibold text-4xl text-white">Settings</Text>
+        </View>
+        <View className="w-full grid grid-cols-1 grid-rows-1 gap-4">
+          <TouchableOpacity
+            className="flex-row justify-between items-center pb-2 border-b-2 border-[#212836]"
+            onPress={() => Linking.openURL("mailto:tim@witzdam.com")}
+          >
+            <View className="flex-row items-center">
+              <Support />
+              <Text className="ml-2 text-white font-medium text-lg">
+                Support
+              </Text>
+            </View>
+            <Arrow fill="white" width="20" height="20" />
+          </TouchableOpacity>
+          <TouchableOpacity
+            className="flex-row justify-between items-center pb-2 border-b-2 border-[#212836]"
+            onPress={() => logOutUser()}
+          >
+            <View className="flex-row items-center">
+              <LogOut />
+              <Text className="ml-2 text-white font-medium text-lg">
+                Log out
+              </Text>
+            </View>
+            <Arrow fill="white" width="20" height="20" />
+          </TouchableOpacity>
+        </View>
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
--- assets/arrow.js
+++ assets/arrow.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { SvgXml } from "react-native-svg";
-export default function SvgTest() {
-  const svg = `<svg width="10" height="8" viewBox="0 0 10 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <path d="M9.35355 4.35355C9.54882 4.15829 9.54882 3.84171 9.35355 3.64645L6.17157 0.464467C5.97631 0.269204 5.65973 0.269204 5.46447 0.464467C5.2692 0.659729 5.2692 0.976311 5.46447 1.17157L8.29289 4L5.46447 6.82843C5.2692 7.02369 5.2692 7.34027 5.46447 7.53553C5.65973 7.7308 5.97631 7.7308 6.17157 7.53553L9.35355 4.35355ZM-3.88546e-08 4.5L9 4.5L9 3.5L3.88546e-08 3.5L-3.88546e-08 4.5Z" fill="black"/>
+export default function SvgTest({
+  width = "10",
+  height = "8",
+  fill = "black",
+}) {
+  const svg = `<svg width="${width}" height="${height}" viewBox="0 0 10 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M9.35355 4.35355C9.54882 4.15829 9.54882 3.84171 9.35355 3.64645L6.17157 0.464467C5.97631 0.269204 5.65973 0.269204 5.46447 0.464467C5.2692 0.659729 5.2692 0.976311 5.46447 1.17157L8.29289 4L5.46447 6.82843C5.2692 7.02369 5.2692 7.34027 5.46447 7.53553C5.65973 7.7308 5.97631 7.7308 6.17157 7.53553L9.35355 4.35355ZM-3.88546e-08 4.5L9 4.5L9 3.5L3.88546e-08 3.5L-3.88546e-08 4.5Z" fill="${fill}"/>
     </svg>`;
 
-  const Arrow = () => <SvgXml xml={svg} width="13px" height="13px" />;
-  return <Arrow />;
+  const SVG = () => <SvgXml xml={svg} width={width} height={height} />;
+  return <SVG />;
 }

658c8c4f86aebb3c972939594760f60a50b7edda
timwitzdam <tim2013w@gmail.com>
2023-08-25 12:56:11 +0200

    Updated padding for ios + android

--- app/_layout.js
+++ app/_layout.js
@@ -1,12 +1,28 @@
 import { Slot } from "expo-router";
 import { RootSiblingParent } from "react-native-root-siblings";
 import { StatusBar } from "expo-status-bar";
+import { View, Platform } from "react-native";
+import { useEffect, useState } from "react";
 
 export default function Layout() {
+  const [paddingTop, setPaddingTop] = useState(null);
+  useEffect(() => {
+    if (Platform.OS === "android") {
+      setPaddingTop("2");
+    } else {
+      setPaddingTop("10");
+    }
+  }, []);
   return (
     <RootSiblingParent>
-      <Slot />
-      <StatusBar style="light" />
+      <View
+        className={`flex-1 bg-[#1B1824] ${
+          Platform.OS === "android" ? "pt-4" : "pt-10"
+        }`}
+      >
+        <Slot />
+        <StatusBar style="light" />
+      </View>
     </RootSiblingParent>
   );
 }
